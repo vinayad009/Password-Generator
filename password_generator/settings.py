34777b7b74bc8b00c20cc985cb975edd7da39679
Vinay Kumar A Deshmukh <94872396+vinayad009@users.noreply.github.com>
2023-06-17 03:23:16 +0530

    Upadte settings.py

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r9u*dgap@ab%puv9%tc&17rdgj&&(998*=nb14dks-v#$_jmuv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
